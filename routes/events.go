ab16e0546df2a75c00a25316a0eada6ba5086956
dunky-star <dunkygeoffrey39@gmail.com>
2024-06-09 14:38:01 -0500

    Implementing Middlewares for route protection

--- routes/events.go
+++ routes/events.go
@@ -64,9 +64,15 @@ func updateEvent(context *gin.Context) {
 		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not fetch event id."})
 		return
 	}
-	_, err = models.GetEventById(eventId)
+	userId := context.GetInt64("userId")
+	event, err := models.GetEventById(eventId)
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch the event."})
+		return
+	}
+
+	if event.UserID != userId {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized to update event."})
 		return
 	}
 
@@ -93,10 +99,17 @@ func deleteEvent(context *gin.Context) {
 		return
 	}
 	event, err := models.GetEventById(eventId)
+	userId := context.GetInt64("userId")
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch the event."})
 		return
 	}
+
+	if event.UserID != userId {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized to update event."})
+		return
+	}
+
 	err = event.Delete()
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not delete the event."})
